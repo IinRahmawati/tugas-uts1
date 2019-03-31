d06e801eb219ec913a0e055d9a4fe9704237172e
IinRahmawati <48981585+IinRahmawati@users.noreply.github.com>
2019-03-31 20:33:16 +0700

    Add files via upload

---
+++ uts1iinrahmawati.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char nama [30];
+    char kelas [20];
+    char nim [20];
+    char tahun angkatan[40];
+
+    cout<<"nama mahasiswa : ":
+    cin>>nama;
+    cout<<"kelas : ";
+    cin>>kelas;
+    cout<<"nim  : ";
+    cin>>nim;
+    cout<<"tahun angkatan : ";
+    cin>>tahun angkatan;
+
+    cout<<"nama mahasiswa : "<<nama<<"\n";
+    cout<<"kelas : "<<kelas<< "\n";
+    cout<<"nim   : "<<nim<<"\n";
+    cout<<"tahun angkatan : "<<tahun angkatan<<"\n";
+
+    return 0;
+
+}
